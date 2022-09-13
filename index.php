d5bc10da879022488526755ddfde63e4bbc5743a
GerbenVenneman <gerbenvenneman@gmail.com>
2022-09-13 10:42:19 +0200

    header main en footer gemaakt
    Het grootste gedeelte staat al. Er moeten nu nog wat kleine aanpassingen komen.

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/main.css">
+	<title>Open-dag</title>
+</head>
+<body>
+	<header>
+		<img src="img/logo.png" alt="">
+		<div class="h1">
+			<h1>Open-dag Curio</h1>
+		</div>
+		<nav>
+			<a href="#vakken">Vakken</a>
+			<a href="#docenten">Docenten</a>
+			<a href="#rooster">Rooster</a>
+			<a href="#vakanties">Vakanties</a>
+			<a href="#versnellen">Versnellen</a>
+			<a href="#blokken">Blokken</a>
+		</nav>
+	</header>
+	<main>
+		<div id="vakken" class="vakken">
+			<h2>Vakken</h2>
+			<div class="alleVakken">	
+				<div class="sub-vakken">
+					<h3>Generiek</h3>
+					<p>De generieke vakken bestaan uit Engels, Nederlands en Rekenen.</p>
+				</div>
+				<div class="sub-vakken">
+					<h3>Web</h3>
+					<p>Dit is een praktijkvak waar je alles leert over websites bouwen en onderhouden.</p>
+				</div>
+				<div class="sub-vakken">
+					<h3>Native</h3>
+					<p>Dit is een praktijvak waar je alles leert over Windows. Hier ga je leren om apps te maken.</p>
+				</div>
+				<div class="sub-vakken">
+					<h3>Pra</h3>
+					<p>Dit is een praktijkvak waar je alles van Native en Web toepast</p>
+				</div>
+				<div class="sub-vakken">
+					<h3>Pro</h3>
+					<p>Dit vak geeft ondersteuning aan de praktijk en leert je om te presenteren, interviewen en jezelf te reflecteren.</p>
+				</div>
+				<div class="sub-vakken">
+					<h3>Studieloopbaanbegelieding</h3>
+					<p>Bij dit vak heb je een moment met je mentor waar je klassikaal kijkt hoe het met iedereen gaat, of het huiswerk af is en of je vaak afwezig bent geweest. Ook ga je orienterende opdrachten maken voor later</p>
+				</div>
+			</div>
+		</div>
+		<div class="docenten">
+			<h2>Docenten</h2>
+			<div class="alleDocenten">
+				<div class="sub-docenten">
+			         <img src="img/docenten.png" alt="">
+			         <p>Joost</p>
+			     </div>
+			     <div class="sub-docenten">
+				     <img src="img/docenten.png" alt="">
+			         <p>Ine</p>
+			     </div>
+			     <div class="sub-docenten">
+				      <img src="img/docenten.png" alt="">
+			          <p>Bart</p>
+			     </div>
+			</div>
+			
+		</div>
+		<div id="rooster" class="rooster">
+			<h2>Rooster</h2>
+			<img src="img/rooster3.png" alt="">
+		</div>
+		<div id="vakanties" class="vakanties">
+			<h2>Vakanties</h2>	
+			<p>Zoals elke school hebben ook wij op het Curio vakanties.</p>	
+			<div class="tekstkleur">	
+                 <p>Zie het overzicht hieronder:</p>
+                 <img src="img/vakanties.png" alt="">
+			</div>	
+		</div>
+		<div id="versnellen" class="versnellen">
+			<h2>Versnellen</h2>
+			<p>Ook is er bij ons de optie om te versnellen. Dit betekend dat je de opleiding in 3 jaar af kan ronden in plaats van 4 jaar.</p>
+		</div>
+		<div id="blokken" class="blokken">
+			<h2>Blokken</h2>
+			<p>Wij werken met blokken van een half jaar. In leerjaar 1 heb je dus blok A en B. De overgang wordt bepaald per blok.</p>
+		</div>	
+	</main>
+	<footer>
+		<h2>Contactmogelijkheden:</h2>
+		<div class="contact-locatie">
+			<div class="sub-contact-locatie">
+				<h3>Contact</h3>
+				<li>Telefoonnummer: 088-2098250</li>
+				<h3>Locatie</h3>
+				<li>Terheijdenseweg 350, 4826 AA Breda</li>
+			</div>
+			
+			<img src="img/gebouw.png" alt="">
+		</div>
+		
+
+	</footer>
+</body>
+</html>
